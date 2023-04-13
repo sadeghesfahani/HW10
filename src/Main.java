aff2e1ef6fcf04fd3933c33d88a120c0125057a7
Sina <sadeghesfahani.sina@gmail.com>
2023-04-13 09:21:00 +0430

    Implement test in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pair<Integer,Integer> pair1 = new Pair<>(1,1);
+        Pair<Integer,Integer> pair2 = new Pair<>(2,2);
+        Pair<Integer,Integer> pair3 = new Pair<>(3,3);
+
+        Map<Integer,Integer> map1 = new Map<>(pair1);
+        Map<Integer,Integer> map2 = new Map<>(pair2);
+        map1.add(pair2);
+        map2.add(pair1);
+
+        System.out.println(map1.equals(map2));
+
     }
 }
