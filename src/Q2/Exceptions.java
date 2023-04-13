6c5a9bf9ed794d175bac9562cd6de6e427652c3f
Sina <sadeghesfahani.sina@gmail.com>
2023-04-13 09:30:24 +0430

    implement throws in one catch

--- src/Q2/Exceptions.java
+++ src/Q2/Exceptions.java
@@ -15,33 +15,51 @@ class ExceptionB extends ExceptionA {
 
 public class Exceptions {
     public static void main(String[] args) {
-        try {
-            // throw an ExceptionA
-            throw new ExceptionA("This is an ExceptionA.");
-        } catch (Exception exception) {
-            System.out.println("Caught an exception: " + exception.getMessage());
-        }
+//        try {
+//            // throw an ExceptionA
+//            throw new ExceptionA("This is an ExceptionA.");
+//        } catch (Exception exception) {
+//            System.out.println("Caught an exception: " + exception.getMessage());
+//        }
+//
+//        try {
+//            // throw an ExceptionB
+//            throw new ExceptionB("This is an ExceptionB.");
+//        } catch (Exception exception) {
+//            System.out.println("Caught an exception: " + exception.getMessage());
+//        }
+//
+//        try {
+//            // throw a NullPointerException
+//            String s = null;
+//            s.length();
+//        } catch (Exception exception) {
+//            System.out.println("Caught an exception: " + exception.getMessage());
+//        }
+//
+//        try {
+//            // throw an IOException
+//            throw new IOException("This is an IOException.");
+//        } catch (Exception exception) {
+//            System.out.println("Caught an exception: " + exception.getMessage());
+//        }
+//    }
 
-        try {
-            // throw an ExceptionB
-            throw new ExceptionB("This is an ExceptionB.");
-        } catch (Exception exception) {
-            System.out.println("Caught an exception: " + exception.getMessage());
-        }
-
-        try {
-            // throw a NullPointerException
-            String s = null;
-            s.length();
-        } catch (Exception exception) {
-            System.out.println("Caught an exception: " + exception.getMessage());
-        }
-
-        try {
-            // throw an IOException
-            throw new IOException("This is an IOException.");
-        } catch (Exception exception) {
-            System.out.println("Caught an exception: " + exception.getMessage());
-        }
+//        try {
+//            // Throw various exceptions
+//            throw new ExceptionA("ExceptionA");
+//            throw new ExceptionB("ExceptionB");
+//            throw new NullPointerException("NullPointerException");
+//            throw new IOException("IOException");
+//        } catch (ExceptionB exception) {
+//            System.out.println("Caught ExceptionB: " + exception.getMessage());
+//        } catch (ExceptionA exception) {
+//            System.out.println("Caught ExceptionA: " + exception.getMessage());
+//        } catch (NullPointerException exception) {
+//            System.out.println("Caught NullPointerException: " + exception.getMessage());
+//        } catch (IOException exception) {
+//            System.out.println("Caught IOException: " + exception.getMessage());
+//        }
     }
+
 }
